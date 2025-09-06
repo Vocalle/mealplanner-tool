799fb8e5ec4fca69151b75820897b06e8a6309ac
Vocalle <twentysixinstinct@gmail.com>
2025-09-06 12:58:53 +0200

    Create translations.py

---
+++ meal_planner/translations.py
@@ -0,0 +1,25 @@
+# translations.py
+
+DE = {
+    "title": "Mahlzeitenplaner",
+    "add_recipe": "Rezept hinzufügen",
+    "save": "Speichern",
+    "language_toggle": "Sprache wechseln zu Englisch",
+    "weekday": "Wochentag auswählen",
+    "recipe_name": "Rezeptname",
+    "ingredients": "Zutaten",
+    "instructions": "Zubereitung",
+    "submit": "Eintragen"
+}
+
+EN = {
+    "title": "Meal Planner",
+    "add_recipe": "Add Recipe",
+    "save": "Save",
+    "language_toggle": "Switch language to German",
+    "weekday": "Select weekday",
+    "recipe_name": "Recipe name",
+    "ingredients": "Ingredients",
+    "instructions": "Instructions",
+    "submit": "Submit"
+}
